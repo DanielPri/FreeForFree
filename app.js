763fb1376db71a622f56e49e5822cfe11adb8e8d
Mikael Samvelian <mikael.samvelian@gmail.com>
2018-09-27 01:15:35 -0400

    Add pug and other packages
    This commit adds a couple of packages that we are plannig to use in the future for our web-app, sets our view engine to "pug" and renders the index "pug" file in the views directory.

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-// Simple "hello world" from GoogleCloudPlatform tutorials
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', (req, res) => {
-  res.status(200).send('FreeForFree!');
+  res.render('index');
+});
+
+// Basic 404 handler -> found from google cloud tutorials
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Basic error handler -> found from google cloud tutorials
+app.use((err, req, res, next) => {
+  console.error(err);
+  // If our routes specified a specific response, then send that. Otherwise,
+  // send a generic message so as not to leak anything.
+  res.status(500).send(err.response || 'Something broke!');
 });
 
 if (module === require.main) {
