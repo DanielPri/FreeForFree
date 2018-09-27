fd937b5f726a5687ecb8a7410f62b28ae4d089c7
Mikael Samvelian <mikael.samvelian@gmail.com>
2018-09-27 15:02:41 -0400

    Add google cloud sql config and crud
    This commit adds the necessary config files and sample crud functions that google provides in the following tutorial: https://cloud.google.com/nodejs/getting-started/tutorial-app

---
+++ books/crud.js
@@ -0,0 +1,62 @@
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+function getModel () {
+  return require(`./model-${require('../config').get('DATA_BACKEND')}`);
+}
+
+const router = express.Router();
+
+// Automatically parse request body as form data
+router.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Content-Type for all responses for these routes
+router.use((req, res, next) => {
+  res.set('Content-Type', 'text/html');
+  next();
+});
+
+/**
+ * GET /users
+ *
+ * Display a page of users (up to ten at a time).
+ */
+router.get('/', (req, res, next) => {
+  getModel().list(1, 10, req.query.pageToken, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.render('users/list.pug', {
+      users: entities,
+      nextPageToken: cursor
+    });
+  });
+});
+
+/**
+ * Errors on "/users/*" routes.
+ */
+router.use((err, req, res, next) => {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = err.message;
+  next(err);
+});
+
+module.exports = router;
